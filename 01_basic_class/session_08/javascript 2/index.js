1c6fe8f54e495639ee9c8d6868235689514c12f7
ahmdabrr <ahmad.abrar@mail.ugm.ac.id>
2020-04-21 13:23:18 +0800

    session 8

---
+++ 01_basic_class/session_08/javascript 2/index.js
@@ -0,0 +1,58 @@
+function penjumlahan (numb1, numb2) {
+    var hasil = numb1 + numb2;
+    return hasil;
+}
+
+var numb1 = 6
+var numb2 = 2
+var hasil = numb1 + numb2
+
+console.log ('hasilnya adalah : ' + penjumlahan(6, 2));
+console.log ('Hasilnya adalah : ' + hasil);
+
+function pengurangan (numb3, numb4) {
+    var hasil = numb3 - numb4;
+    return hasil;
+}
+
+var numb3 = 7
+var numb4 = 2
+var hasil = numb3 - numb4
+
+console.log ('hasilnya adalah : ' + pengurangan(7, 2));
+
+
+function pembagian (numb5, numb6) {
+    var hasil = numb5 / numb6;
+    return hasil;
+}
+
+var numb5 = 8
+var numb6 = 2
+var hasil = numb5 / numb6
+
+console.log ('hasilnya adalah : ' + pembagian(8, 2));
+
+
+function perkalian (numb7, numb8) {
+    var hasil = numb7 * numb8;
+    return hasil;
+}
+
+var numb7 = 9
+var numb8 = 2
+var hasil = numb7 * numb8
+
+console.log ('hasilnya adalah : ' + perkalian(9, 2));
+
+
+function modulus (numb9, numb10) {
+    var hasil = numb9 % numb10;
+    return hasil;
+}
+
+var numb9 = 4
+var numb10 = 2
+var hasil = numb9 % numb10
+
+console.log ('hasilnya adalah : ' + modulus(4, 2));
